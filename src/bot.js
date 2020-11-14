b79a77bbf5d8c232d8d3a11bc0d74ec46c415456
CriticalMax <m.belohoubek@outlook.com>
2020-11-15 00:06:09 +0100

    Initial

--- src/bot.js
+++ src/bot.js
@@ -10,33 +10,32 @@ client.on('ready', () => {
 
 client.on('message', (message) => {
     if(message.author.bot) {return}
-
+    
     const messageSplit = message.content.split(' ');
+    if(messageSplit[0] !== '!bot') {return}
+    
+    switch(messageSplit[1]) {
+        case 'w2g':
+            if (messageSplit[2] === null || messageSplit[2] === undefined || messageSplit[2] === "") {return}
+            const body = { 
+                "w2g_api_key" : process.env.W2G_KEY,
+                "share" : messageSplit[2],
+                "bg_color" : "#383838",
+                "bg_opacity" : "50"
+            }
 
-    if(messageSplit[0] === '!bot') {
-        switch(messageSplit[1]) {
-            case 'w2g':
-                if (messageSplit[2] === null || messageSplit[2] === undefined || messageSplit[2] === "") {return}
-                const body = { 
-                    "w2g_api_key" : process.env.W2G_KEY,
-                    "share" : messageSplit[2],
-                    "bg_color" : "#383838",
-                    "bg_opacity" : "50"
-                }
-
-                fetch ('https://w2g.tv/rooms/create.json', {
-                        method: 'POST',
-                        headers: {
-                          'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify(body),
-                      })
-                        .then(response => response.json())
-                        .then((data) => {
-                            message.channel.send(`https://w2g.tv/rooms/${data.streamkey}`);
-                        });
-                return;
-        }
+            fetch ('https://w2g.tv/rooms/create.json', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(body),
+                    })
+                    .then(response => response.json())
+                    .then((data) => {
+                        message.channel.send(`https://w2g.tv/rooms/${data.streamkey}`);
+                    });
+            return;
     }
 });
 
